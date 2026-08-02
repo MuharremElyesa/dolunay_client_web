5413c6b9b5cff7c0b2edb05fe0cbdb1d2d4d4435
MuharremElyesa <muharremelyesa1453@gmail.com>
2026-08-02 09:44:39 +0000

    Google ile gelen veriler consol'da görüntüleniyor.

--- js/login.js
+++ js/login.js
@@ -2,3 +2,18 @@ function signInGoogle() {
     // Doğrudan Rust backend OAuth rotamıza yönlendiriyoruz
     window.location.href = "http://api.dolunay.muharremelyesa.com/auth/google/login";
 }
+
+const urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.get('status') === 'success') {
+    const email = urlParams.get('email');
+    const firstName = urlParams.get('first_name');
+    const lastName = urlParams.get('last_name');
+    const picture = urlParams.get('picture');
+
+    console.log("Kullanıcı Bilgileri:", { email, firstName, lastName, picture });
+
+    // Örnek: Profil resmini ekrandaki img etiketine basmak
+    // document.getElementById('user-avatar').src = picture;
+    // document.getElementById('user-name').innerText = `${firstName} ${lastName}`;
+}
